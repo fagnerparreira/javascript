ea4d6f1920f1ff7db30d3b8a8ad33526aa56b189
Fagner Parreira <fagnerparreira1@gmail.com>
2021-01-11 21:27:03 -0300

    11/01

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+
+/*var d = new Date();
+alert (d.getDay()+1);
+*/
+//alert (d);
+
+
+
+/*var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+
+*/
+
+/*var count = 0;
+while (count <= 5);{
+    console.log(count); 
+    alert(count);
+    count++;
+    
+};
+não funcionou
+*/
+
+
+
+/*
+var idade = prompt ("Qual sua idade");
+if  (idade >= 18){
+    alert(" maior de idade");
+}else {
+    alert("menor de idade");
+};
+
+*/
+
+
+
+/*var frutas = [{ nome: "maça", cor: "vermelha"}, { nome: "uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[0].nome);
+*/
+
+/*var fruta = { nome: "maça", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop()
+//console.log(lista.reverse());
+
+//console.log(lista);
+//console.log(lista.toString()[0]);
+//console.log(lista.join("    "));
+//alert(lista[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+//var nome = "Fagner Parreira";
+//var n1 = 5;
+//var n2 = 4;
+//var frase = "Sou estudante de js"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade +idade2);
+//console.log(nome)
+//console.log(n1 * n2);
+//console.log(frase.toLocaleUpperCase());
